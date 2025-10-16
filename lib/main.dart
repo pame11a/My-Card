923f268e23ecfa7269a8893b0b0418cada6047e0
pame11a <pmellaf29@gmail.com>
2025-10-16 13:58:22 -0300

    Iniciando o codigo

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,28 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF000945),
-        body: Container(),
-      ),
-    ),
+    MeuApp()
   );
-
-
 }
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          backgroundColor: const Color(0xFF000945),
+          body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: Text('Olá Pessoal'),
+            ),
+          ),
+      ),
+    );
+  }
+}
+
