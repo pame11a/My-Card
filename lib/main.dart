a0e02751a9b7844a893223c91a8f547765845eb1
pame11a <pmellaf29@gmail.com>
2025-10-16 12:41:34 -0300

    Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF000945),
+        body: Container(),
+      ),
+    ),
+  );
+
+
+}
