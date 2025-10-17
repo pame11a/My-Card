97fa0081eb6037ac3e19dc5c5f3babced5d7a2a9
pame11a <pmellaf29@gmail.com>
2025-10-17 18:32:31 -0300

    Add os container

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main(){
-  runApp(
-    MeuApp()
-  );
+void main() {
+  runApp(MeuApp());
 }
+
+const Color corAzulPersonalizada = Color(0xFF000945);
 
 class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Scaffold(
-          backgroundColor: const Color(0xFF000945),
-          body: SafeArea(
-            child: Container(
-              padding: EdgeInsets.all(20.0),
-              margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-              height: 100.0,
-              width: 100.0,
-              color: Colors.white,
-              child: Text('Olá Pessoal'),
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: corAzulPersonalizada,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 100.0, //altura
+                width: 100.0, //largura
+                color: Colors.white,
+                child: Text('Container one'),
+              ),
+              SizedBox(
+                height: 20.0, //altura
+              ),
+              Container(
+                height: 100.0, //altura
+                width: 100.0, //largura
+                color: Colors.white,
+                child: Text('Container two'),
+              ),
+              SizedBox(
+                height: 20.0, //altura
+              ),
+              Container(
+                height: 100.0, //altura
+                width: 100.0, //largura
+                color: Colors.white,
+                child: Text('Container Three'),
+              ),
+              SizedBox(
+                height: 20.0, //altura
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
 }
-
